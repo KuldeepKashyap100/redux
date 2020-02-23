889b648353cb72dc17f45096d1ef91202dad8cf7
kuldeep <kuldeepkashyap100@gmail.com>
2020-02-23 22:13:41 +0530

    added store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reduer from "./store/reducer";
+
+const store = createStore(reduer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+
+const initialState={
+    counter:0
+}
+const reduer=(state=initialState,action)=>{
+    return state;
+}
+
+export default reduer;
